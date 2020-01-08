ba695b1572948cde81b80c0f8ce645da2491ef21
tiffhuddleston <tiffhuddleston@gmail.com>
2020-01-07 22:13:42 -0600

    Functional but not pretty

--- script.js
+++ script.js
@@ -1,103 +1,106 @@
-// Objects - Quiz itself, changing questions
-// Actions- show questions, change questions, keep score, register selections, count questions(?) show final score
-// Create variables for html elements
-// variable for score
-// let score = 0
-
-// // function to build quiz score, questions, question number
-// function trivia() {
-
-// }
-
-// function buildQuestions() {
-
-// }
-
-// function buildTrivia() {
-//     if(){
-
-//     } else {
-
-//     }
-// }
-// Function to show results
-// Display the questions
-// Show results on click, submit the answer and return the number correct out of total number
-
-// Change to oop, can I use function and delete a line of code here?
-class TriviaQuestion {
-  constructor(question, options, correctAnswer) {
-    this.question = question;
-    this.options = options;
-    this.correctAnswer = correctAnswer;
-  }
-}
-TriviaQuestion.prototype.correctAnswer = function(selection) {
-  this.correctAnswer === selection;
-};
-
-const questions = [
+// Build Questions
+const allQuestions = [
   new TriviaQuestion(
     'What planet is Dr. Who from?',
     ['Jupiter', 'Galifrey', 'Time Lord Home World', 'Titan 1'],
-    'Galifrey'
+    1
   ),
-  new TriviaQuestion(),
-  new TriviaQuestion()
+  new TriviaQuestion('Who likes bones?', ['Dogs', 'Cats', 'Birds'], 0),
+  new TriviaQuestion('What is best?', ['Sleep', 'Food', 'Work'], 0)
 ];
 
-console.log(question1);
+// Build question object
+function TriviaQuestion(question, options, answer) {
+  this.question = question;
+  this.options = options;
+  this.answer = answer;
+}
+TriviaQuestion.prototype.correctAnswer = function(choice) {
+  return choice === this.answer;
+};
 
-// Create quiz questions- array of object literals
-// const TriviaQuestions = [
-//     {
-//         question: 'What planet is Dr. Who from?',
-//         options: {
-//             a: 'Jupiter',
-//             b: 'Galifrey',
-//             c: 'Time Lord Home World',
-//         },
-//         correctAnswer: 'b'
-//     }
-//     {
-//         question: '',
-//         options: {
-//             a: '',
-//             b: '',
-//             c: '',
-//         },
-//         correctAnswer: 'c'
-//     }
-//     {
-//         question: '',
-//         options: {
-//             a: '',
-//             b: '',
-//             c: '',
-//         },
-//         correctAnswer: 'c'
-//     }
-//         {
-//         question: '',
-//         options: {
-//             a: '',
-//             b: '',
-//             c: '',
-//         },
-//         correctAnswer: 'c'
-//     }
-// ]
+// Build Quiz Object
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
+}
 
-// way to select an answer- button? checkbox? circle(radio)? or input letter in a form?
+Quiz.prototype.buildQuestion = function(index) {
+  var title_div = document.getElementById('question_title');
+  var answer_radios = document.getElementById('answer_radio_group');
 
-// loop to iterate through question array for loop
-// for (i = 0; i < TriviaQuestions.length; i++) {
-//     let response = 'click'
-//     if (response == correct answer) {
-//         return 'correct'
-//     } else {
-//         return 'wrong'
-//     }
-// }
+  var question = this.questions[index];
+  title_div.innerHTML = question.question;
+  var radioHtml = '';
+  var optionIndex = 0;
+  question.options.forEach(
+    option => (
+      (radioHtml += `
+        <input 
+            type='radio' 
+            name='question'
+            value=${optionIndex}>
+            ${option}
+        </input>
+        <br>
+    `),
+      (optionIndex += 1)
+    )
+  );
+  answer_radios.innerHTML = radioHtml;
+};
 
-// buttons-
+Quiz.prototype.getQuestionIndex = function() {
+  return this.questions[this.questionIndex];
+};
+
+Quiz.prototype.isOver = function() {
+  return this.questions.length === this.questionIndex;
+};
+
+Quiz.prototype.guess = function(answer) {
+  if (this.getQuestionIndex().correctAnswer(answer)) {
+    this.score++;
+    let keepScore = document.getElementById('running_score');
+    keepScore.innerHTML = `${this.score}/${allQuestions.length}`;
+  }
+  this.questionIndex++;
+  if (this.isOver()) {
+    let scoreLabel = document.getElementById('score_label');
+    scoreLabel.innerHTML = 'Final Score';
+    document.getElementById('begin_quiz_btn').hidden = false;
+
+    return;
+  }
+  this.buildQuestion(this.questionIndex);
+};
+
+var theQuiz = new Quiz(allQuestions);
+
+// Build quiz- populate the questions
+function buildQuiz() {
+  console.log('buildQuiz fired');
+
+  this.theQuiz.buildQuestion(0);
+  document.getElementById('answer_btn').hidden = false;
+  document.getElementById('begin_quiz_btn').hidden = true;
+  let keepScore = document.getElementById('running_score');
+  keepScore.innerHTML = `0/${allQuestions.length}`;
+}
+
+function processAnswer() {
+  console.log('processAnswer fired');
+  var answer = '';
+  var radio_group = document.getElementById('answer_radio_group');
+  var radios = radio_group.getElementsByTagName('input');
+  for (var i = 0; i < radios.length; ++i) {
+    if (radios[i].checked) {
+      console.log(`answer found: ${radios[i].value}`);
+      answer = parseInt(radios[i].value);
+      break;
+    }
+  }
+  console.log(`answer: ${answer}`);
+  theQuiz.guess(answer);
+}
